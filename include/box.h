b0a178b9ab02903c270a604dfeaaf7e1c4f1daab
Laurent Brusa <wrmultitudes@gmail.com>
2024-09-15 12:32:44 +0200

    FEAT add print box

--- include/box.h
+++ include/box.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   box.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: lbrusa <lbrusa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:38:14 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/07/26 10:17:03 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/09/15 12:29:59 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,30 @@
 #include "quad.h"
 #include "material.h"
 #include "hittable_list.h"
+#include "color.h"
 
 typedef struct	s_box {
-	t_hittable  base;
-    t_quad 		q1;
-    t_quad 		q2;
-    t_quad 		q3;
-    t_quad 		q4;
-    t_quad 		q5;
-    t_quad 		q6;
+	t_hittable      base;
+    t_point3        a;
+    t_point3        b;
+    t_quad 		    q1;
+    t_quad 		    q2;
+    t_quad 		    q3;
+    t_quad 		    q4;
+    t_quad 		    q5;
+    t_quad 		    q6;
+    t_rgb           rgb;
+    t_color			color;
+	t_lambertian 	lambertian_mat;
+	t_solid_color 	texture;
+    t_material		*mat;
+    void		    (*print)(const void* self);
 }				t_box;
 
-
-t_box box(t_point3 a, t_point3 b, t_material *mat);
-void create_box(t_box *box, t_point3 a, t_point3 b, t_material *mat);
-bool hit_box(const void* self, const t_ray *r, t_interval ray_t,  t_hit_record *rec);
+t_box   box(t_point3 a, t_point3 b, t_material *mat);
+t_box   box_rgb(t_point3 a, t_point3 b, t_rgb rgb);
+// void    create_box(t_box *box, t_point3 a, t_point3 b, t_material *mat);
+void    print_box(const void *self);
+bool    hit_box(const void* self, const t_ray *r, t_interval ray_t,  t_hit_record *rec);
 
 #endif
--- src/box.c
+++ src/box.c
@@ -6,10 +6,11 @@
 /*   By: lbrusa <lbrusa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:40:26 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/09/14 17:30:54 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/09/15 12:29:56 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "box.h"
 #include "vec3.h"
 #include "quad.h"
@@ -17,10 +18,48 @@
 #include "hittable_list.h"
 #include "hittable.h"
 
+t_box box_rgb(t_point3 a, t_point3 b, t_rgb rgb)
+{
+	t_box box;
+    box.a = a;
+    box.b = b;
+	t_point3 min = point3(fmin(a.x, b.x), fmin(a.y, b.y), fmin(a.z, b.z));
+    t_point3 max = point3(fmax(a.x, b.x), fmax(a.y, b.y), fmax(a.z, b.z));
+
+    t_vec3 dx = vec3(max.x - min.x, 0, 0);
+    t_vec3 dy = vec3(0, max.y - min.y, 0);
+    t_vec3 dz = vec3(0, 0, max.z - min.z);
+
+    box.q1 = quad_rgb(point3(min.x, min.y, max.z),  dx,  dy, rgb); // front
+    box.q2 = quad_rgb(point3(max.x, min.y, max.z), vec3negate(dz),  dy, rgb); // right
+    box.q3 = quad_rgb(point3(max.x, min.y, min.z), vec3negate(dx),  dy, rgb); // back
+    box.q4 = quad_rgb(point3(min.x, min.y, min.z),  dz,  dy, rgb); // left
+    box.q5 = quad_rgb(point3(min.x, max.y, max.z),  dx, vec3negate(dz), rgb); // top
+    box.q6 = quad_rgb(point3(min.x, min.y, min.z),  dx,  dz, rgb); // bottom
+    box.rgb = rgb;
+
+// i use the color to create a texture
+	solid_color_init(&(box.texture), box.color);
+	// i init the lambertian material with the texture
+	lambertian_init_tex(&(box.lambertian_mat), (t_texture*)&(box.texture));
+	// i assign the material to the sphere as a pointer
+	// the pointer will contain the scatter function for the material
+	// which will be passed to the t_record struct when hit
+ 	box.mat = (t_material*)&(box.lambertian_mat); 
+
+	box.base.hit = hit_box;
+    box.base.pdf_value = obj_pdf_value;
+    box.base.random = obj_random;
+    box.print = print_box;  
+
+	return (box);
+}
+
 t_box box(t_point3 a, t_point3 b, t_material *mat)
 {
 	t_box box;
-
+    box.a = a;
+    box.b = b;
 	t_point3 min = point3(fmin(a.x, b.x), fmin(a.y, b.y), fmin(a.z, b.z));
     t_point3 max = point3(fmax(a.x, b.x), fmax(a.y, b.y), fmax(a.z, b.z));
 
@@ -34,30 +73,37 @@ t_box box(t_point3 a, t_point3 b, t_material *mat)
     box.q4 = quad(point3(min.x, min.y, min.z),  dz,  dy, mat); // left
     box.q5 = quad(point3(min.x, max.y, max.z),  dx, vec3negate(dz), mat); // top
     box.q6 = quad(point3(min.x, min.y, min.z),  dx,  dz, mat); // bottom
-
+    box.rgb = rgb(0, 0, 0);
 	box.base.hit = hit_box;
     box.base.pdf_value = obj_pdf_value;
     box.base.random = obj_random;
+    box.print = print_box;  
 
 	return (box);
 }
 
 
-void create_box(t_box *box, t_point3 a, t_point3 b, t_material *mat)
+// void create_box(t_box *box, t_point3 a, t_point3 b, t_material *mat)
+// {
+//   	t_point3 min = point3(fmin(a.x, b.x), fmin(a.y, b.y), fmin(a.z, b.z));
+//     t_point3 max = point3(fmax(a.x, b.x), fmax(a.y, b.y), fmax(a.z, b.z));
+
+//     t_vec3 dx = vec3(max.x - min.x, 0, 0);
+//     t_vec3 dy = vec3(0, max.y - min.y, 0);
+//     t_vec3 dz = vec3(0, 0, max.z - min.z);
+
+//     box->q1 = quad(point3(min.x, min.y, max.z),  dx,  dy, mat); // front
+//     box->q2 = quad(point3(max.x, min.y, max.z), vec3negate(dz),  dy, mat); // right
+//     box->q3 = quad(point3(max.x, min.y, min.z), vec3negate(dx),  dy, mat); // back
+//     box->q4 = quad(point3(min.x, min.y, min.z),  dz,  dy, mat); // left
+//     box->q5 = quad(point3(min.x, max.y, max.z),  dx, vec3negate(dz), mat); // top
+//     box->q6 = quad(point3(min.x, min.y, min.z),  dx,  dz, mat); // bottom
+// }
+void    print_box(const void *self)
 {
-  	t_point3 min = point3(fmin(a.x, b.x), fmin(a.y, b.y), fmin(a.z, b.z));
-    t_point3 max = point3(fmax(a.x, b.x), fmax(a.y, b.y), fmax(a.z, b.z));
-
-    t_vec3 dx = vec3(max.x - min.x, 0, 0);
-    t_vec3 dy = vec3(0, max.y - min.y, 0);
-    t_vec3 dz = vec3(0, 0, max.z - min.z);
-
-    box->q1 = quad(point3(min.x, min.y, max.z),  dx,  dy, mat); // front
-    box->q2 = quad(point3(max.x, min.y, max.z), vec3negate(dz),  dy, mat); // right
-    box->q3 = quad(point3(max.x, min.y, min.z), vec3negate(dx),  dy, mat); // back
-    box->q4 = quad(point3(min.x, min.y, min.z),  dz,  dy, mat); // left
-    box->q5 = quad(point3(min.x, max.y, max.z),  dx, vec3negate(dz), mat); // top
-    box->q6 = quad(point3(min.x, min.y, min.z),  dx,  dz, mat); // bottom
+    t_box *box = (t_box *)self;
+    printf("box\t\t%.f,%.f,%.f\t\t%.f,%.f,%.f\t\t%d,%d,%d\n", 
+    box->a.x, box->a.y, box->a.z,box->b.x, box->b.y, box->b.z, box->rgb.r, box->rgb.g, box->rgb.b);
 }
 
 bool hit_box(const void* self, const t_ray *r, t_interval ray_t,  t_hit_record *rec)
