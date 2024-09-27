41c0ec5fb84a609699b25736243d78806c25a559
Laurent Brusa <wrmultitudes@gmail.com>
2024-09-27 12:58:04 +0200

    ADD refactor for checker size

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:22:36 by ralgaran          #+#    #+#             */
-/*   Updated: 2024/09/27 12:27:02 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/09/27 12:57:52 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -316,7 +316,7 @@ static void	get_sphere(t_objects *obj)
 	}
 	else if (count_tokens(tokens) == 5)
 	{
-		checker_texture_init(&obj->spheres[set_index].checker, 20, set_rgb(obj, 3, "sphere"), set_rgb(obj, 4, "sphere"));
+		checker_texture_init(&obj->spheres[set_index].checker, CHECKER_SIZE, set_rgb(obj, 3, "sphere"), set_rgb(obj, 4, "sphere"));
 		lambertian_init_tex(&obj->spheres[set_index].lambertian_mat, (t_texture *)&obj->spheres[set_index].checker);
 		sphere_mat(&obj->spheres[set_index], set_vec3(obj, 1, "sphere", 0), diam, \
 			(t_material *) &obj->spheres[set_index].lambertian_mat);
