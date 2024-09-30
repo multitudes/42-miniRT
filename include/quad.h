b2247db7872087bf9c28b1b4eba38a38dbf6c528
Laurent Brusa <wrmultitudes@gmail.com>
2024-09-30 09:16:45 +0200

    NORM! vec3.h

--- include/quad.h
+++ include/quad.h
@@ -13,11 +13,11 @@
 #ifndef QUAD_H
 # define QUAD_H
 
-#include "vec3.h"
-#include "interval.h"
-#include "material.h"
-#include "ray.h"
-#include "hittable.h"
+# include "vec3.h"
+# include "interval.h"
+# include "material.h"
+# include "ray.h"
+# include "hittable.h"
 
 /*
  * A quad is a 4-pointed polygon in 3D space
@@ -27,9 +27,11 @@
  * v: the second vector of the quad
  * mat: the material of the quad
  *
- * The miniRT quad is defined by the center point q
- * and a normalized vector and a color. This below is from the book
- * but can be adapted to the miniRT quad.
+ * The miniRT quad is not in the mandatory part and 
+ * could be also be defined by the center point q
+ * and a normalized vector and a color. 
+ * This below is from the book
+ * but can be adapted further.
  */
 typedef struct s_quad
 {
@@ -58,7 +60,6 @@ void 	quad_mat(t_quad *qd, t_point3 q, t_vec3 u, t_vec3 v, t_material *mat);
 void	print_quad(const void *self);
 bool	hit_quad(const void* self, const t_ray *r, t_interval ray_t,  t_hit_record *rec);
 bool	is_interior(double a, double b, t_hit_record *rec);
-
 double quad_pdf_value(const void *self, const t_point3 *orig, const t_vec3 *dir);
 t_vec3 quad_random(const void *self, const t_point3 *orig);
 
--- include/vec3.h
+++ include/vec3.h
@@ -1,8 +1,7 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vec3.h                                   		    :+:      :+:    :+:   */
+/*   vec3.h                               				:+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -11,53 +10,55 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_H
-# define VECTOR_H
+#ifndef VEC3_H
+# define VEC3_H
 
-#include <stdbool.h>
+# include <stdbool.h>
 
-typedef struct 	s_vec3
+typedef union s_vec3
 {
-	union {
-        struct {
-            double x;
-            double y;
-            double z;
-        };
-        struct {
-            double r;
-            double g;
-            double b;
-        };
+	struct
+	{
+		double	x;
+		double	y;
+		double	z;
 	};
-}				t_vec3, t_color, t_point3;
+	struct
+	{
+		double	r;
+		double	g;
+		double	b;
+	};
+}				t_vec3;
 
-t_vec3		vec3(double x, double y, double z);
-t_point3	point3(double x, double y, double z);
+typedef t_vec3	t_color;
+typedef t_vec3	t_point3;
 
-t_vec3		vec3negate(const t_vec3 a);
-t_vec3		vec3add(const t_vec3 a, const t_vec3 b);
-t_vec3		vec3substr(const t_vec3 a, const t_vec3 b);
-t_vec3		vec3mult(const t_vec3 a, const t_vec3 b);
-t_vec3		vec3multscalar(const t_vec3 a, double t);
-t_vec3		vec3divscalar(const t_vec3 a, double t);
-t_vec3		cross(const t_vec3 a, const t_vec3 b);
-double		length_squared(const t_vec3 v);
-double		length(const t_vec3 v);
-void		print_vec3(const t_vec3 *v);
-double		dot(const t_vec3 a, const t_vec3 b);
-t_vec3		random_vec3();
-t_vec3		random_vec3_min_max(double min, double max);
-bool 		near_zero(t_vec3 e); 
-t_vec3 		sample_square();
-t_vec3  	unit_vector(t_vec3 v);
-t_vec3 		random_in_unit_disk();
-t_vec3 		random_in_unit_sphere();
-t_vec3 		random_on_hemisphere(const t_vec3 normal);
-t_vec3 		random_unit_vector();
-t_vec3		random_cosine_direction(void);
-t_vec3		reflect(const t_vec3 v, const t_vec3 n);
-t_vec3 		refract(const t_vec3 uv, const t_vec3 n, double etai_over_etat);
-double 		reflectance(double cosine, double refraction_index);
+t_point3		point3(double x, double y, double z);
+t_vec3			vec3(double x, double y, double z);
+t_vec3			vec3negate(const t_vec3 a);
+t_vec3			vec3add(const t_vec3 a, const t_vec3 b);
+t_vec3			vec3substr(const t_vec3 a, const t_vec3 b);
+t_vec3			vec3mult(const t_vec3 a, const t_vec3 b);
+t_vec3			vec3multscalar(const t_vec3 a, double t);
+t_vec3			vec3divscalar(const t_vec3 a, double t);
+t_vec3			cross(const t_vec3 a, const t_vec3 b);
+double			length_squared(const t_vec3 v);
+double			length(const t_vec3 v);
+void			print_vec3(const t_vec3 *v);
+double			dot(const t_vec3 a, const t_vec3 b);
+t_vec3			random_vec3(void);
+t_vec3			random_vec3_min_max(double min, double max);
+bool			near_zero(t_vec3 v);
+t_vec3			sample_square(void);
+t_vec3			unit_vector(t_vec3 v);
+t_vec3			random_in_unit_disk(void);
+t_vec3			random_in_unit_sphere(void);
+t_vec3			random_on_hemisphere(const t_vec3 normal);
+t_vec3			random_unit_vector(void);
+t_vec3			random_cosine_direction(void);
+t_vec3			reflect(const t_vec3 v, const t_vec3 n);
+t_vec3			refract(const t_vec3 uv, const t_vec3 n, double etai_over_etat);
+double			reflectance(double cosine, double refraction_index);
 
 #endif
