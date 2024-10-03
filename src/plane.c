572554f78dfe17207e6c260a10b77719358773d6
Laurent Brusa <wrmultitudes@gmail.com>
2024-10-03 13:49:41 +0200

    DOCS plane

--- src/plane.c
+++ src/plane.c
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:59:39 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/10/03 13:46:17 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/10/03 13:49:14 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 /**
  * @brief plane - Initialize a plane object
- * 
+ *
  * @param pl the plane object (already on the stack)
  * @param point a point in the plane
  * @param normal the normal vector of the plane
  * @param rgbcol the color of the plane
- * 
+ *
  * The plane is defined by a point and a normal vector to the plane
- * The normal vector is normalized. I use PLANE_MAX to limit the plane to 
+ * The normal vector is normalized. I use PLANE_MAX to limit the plane to
  * avoid wasting resources since the ray will take a long time to hit the plane
  */
 void	plane(t_plane *pl, t_point3 point, t_vec3 normal, t_rgb rgbcol)
@@ -77,13 +77,13 @@ void	print_plane(const void *self)
 
 /**
  * @brief hit_plane - Check if a ray hits a plane
- * 
+ *
  * @param self the plane object
  * @param r the ray
  * @param ray_t the interval of the ray
  * @param rec the hit record
  * @return true if the ray hits the plane
- * 
+ *
  * 	no hit if ray is parallel to the plane
  */
 bool	hit_plane(const void *self, const t_ray *r, t_interval ray_t,
@@ -106,37 +106,53 @@ bool	hit_plane(const void *self, const t_ray *r, t_interval ray_t,
 	return (true);
 }
 
-// /**
-//  * @brief plane_pdf_value - Computes the PDF value for a plane
-//  */
-// double	plane_pdf_value(const void *self, const t_point3 *orig,
-// 		const t_vec3 *dir)
-// {
-// 	const t_plane	*pl = (t_plane *)self;
-// 	t_hit_record	rec;
-// 	const t_ray		r = ray(*orig, *dir);
-// 	double			distance_squared;
-// 	double			cosine;
-// 	double			area;
+/**
+ * @brief plane_pdf_value - Computes the PDF value for a plane
+ * 
+ * @param self the plane object
+ * @param orig the origin point
+ * @param dir the direction vector
+ * 
+ * The function plane_pdf_value computes the probability density function (PDF)
+ * value for a plane. The PDF value is the reciprocal of the solid angle
+ * determining the probability density function (PDF) value for sampling
+ * directions towards a plane. We do not currently use this function in the
+ * raytracer because we do not use the plane as a light source.
+ */
+double	plane_pdf_value(const void *self, const t_point3 *orig,
+		const t_vec3 *dir)
+{
+	const t_plane	*pl = (t_plane *)self;
+	t_hit_record	rec;
+	const t_ray		r = ray(*orig, *dir);
+	double			distance_squared;
+	double			cosine;
+	double			area;
 
-// 	if (!hit_plane(pl, &r, interval(0.001, 1e30), &rec))
-// 		return (0);
-// 	distance_squared = len_sqrd(vec3substr(rec.p, *orig));
-// 	cosine = fabs(dot(*dir, pl->normal));
-// 	area = 1e30;
-// 	return (distance_squared / (cosine * area));
-// }
+	if (!hit_plane(pl, &r, interval(0.001, 1e30), &rec))
+		return (0);
+	distance_squared = len_sqrd(vec3substr(rec.p, *orig));
+	cosine = fabs(dot(*dir, pl->normal));
+	area = 1e30;
+	return (distance_squared / (cosine * area));
+}
 
-// t_vec3	plane_random(const void *self, const t_point3 *orig)
-// {
-// 	const t_plane	*pl = (t_plane *)self;
-// 	double			max_u;
-// 	double			max_v;
-// 	t_vec3			p;
+/**
+ * @brief plane_random - Get a random vector on the plane
+ * 
+ * We do not currently use this function in the
+ * raytracer because we do not use the plane as a light source.
+ */
+t_vec3	plane_random(const void *self, const t_point3 *orig)
+{
+	const t_plane	*pl = (t_plane *)self;
+	double			max_u;
+	double			max_v;
+	t_vec3			p;
 
-// 	max_u = random_double(0, 1) * PLANE_MAX;
-// 	max_v = random_double(0, 1) * PLANE_MAX;
-// 	p = vec3add(pl->q, vec3add(vec3multscalar(pl->u, max_u),
-// 				vec3multscalar(pl->v, max_v)));
-// 	return (vec3substr(p, *orig));
-// }
+	max_u = random_double(0, 1) * PLANE_MAX;
+	max_v = random_double(0, 1) * PLANE_MAX;
+	p = vec3add(pl->q, vec3add(vec3multscalar(pl->u, max_u),
+				vec3multscalar(pl->v, max_v)));
+	return (vec3substr(p, *orig));
+}
