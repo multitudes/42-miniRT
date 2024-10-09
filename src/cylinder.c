ea61e1499851d181c0e9a6d2a59eb3bf91ac32dc
Laurent Brusa <wrmultitudes@gmail.com>
2024-10-09 11:15:59 +0200

    NORM cyl

--- src/cylinder.c
+++ src/cylinder.c
@@ -6,17 +6,17 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 15:07:07 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/10/09 11:06:30 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/10/09 11:14:51 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cylinder.h"
-#include "vec3.h"
-#include "ray.h"
 #include "color.h"
-#include <stdio.h>
+#include "cylinder.h"
 #include "material.h"
+#include "ray.h"
 #include "utils.h"
+#include "vec3.h"
+#include <stdio.h>
 
 /**
  * @brief: initializer for a cylinder uncapped standard material
@@ -28,8 +28,8 @@
  * the cylinder diameter
  * the cylinder height
  * R,G,B colors in range [0,255]
-*/
-void		cylinder_uncapped(t_cylinder *c, t_init_params params)
+ */
+void	cylinder_uncapped(t_cylinder *c, t_init_params params)
 {
 	c->base.hit = hit_cylinder;
 	c->base.pdf_value = obj_cylinder_pdf_value;
@@ -43,8 +43,8 @@ void		cylinder_uncapped(t_cylinder *c, t_init_params params)
 	c->rgb = params.rgbcolor;
 	c->color = rgb_to_color(params.rgbcolor);
 	solid_color_init(&(c->texture), c->color);
-	lambertian_init_tex(&(c->lambertian_mat), (t_texture*)&(c->texture));
- 	c->mat = (t_material*)&(c->lambertian_mat);
+	lambertian_init_tex(&(c->lambertian_mat), (t_texture *)&(c->texture));
+	c->mat = (t_material *)&(c->lambertian_mat);
 	c->print = &print_cylinder;
 }
 
@@ -54,7 +54,7 @@ void		cylinder_uncapped(t_cylinder *c, t_init_params params)
  * the rt file will have the following format:
  *
  */
-void		cylinder_mat_uncapped(t_cylinder *c, t_init_params params)
+void	cylinder_mat_uncapped(t_cylinder *c, t_init_params params)
 {
 	c->base.hit = hit_cylinder;
 	c->base.pdf_value = obj_cylinder_pdf_value;
@@ -75,38 +75,46 @@ void		cylinder_mat_uncapped(t_cylinder *c, t_init_params params)
  * the rt file will have the following format:
  *
  */
-void cylinder_capped(t_cylinder_capped *c, t_init_params params)
+void	cylinder_capped(t_cylinder_capped *c, t_init_params params)
 {
+	t_vec3		axis;
+	t_point3	top_center;
+	t_point3	bottom_center;
+
 	c->base.hit = hit_cylinder_capped;
 	c->base.pdf_value = obj_pdf_value;
 	c->base.random = obj_random;
 	cylinder_uncapped(&c->cylinder_uncapped, params);
-
-	t_vec3 axis = unit_vector(params.normal);
-	t_point3 top_center = vec3add(params.center, vec3multscalar(axis, params.height / 2));
-    t_point3 bottom_center = vec3add(params.center, vec3multscalar(axis, -params.height / 2));
-    params.center = top_center;
+	axis = unit_vector(params.normal);
+	top_center = vec3add(params.center, vec3multscalar(axis, params.height
+				/ 2));
+	bottom_center = vec3add(params.center, vec3multscalar(axis, -params.height
+				/ 2));
+	params.center = top_center;
 	disk(&c->top, params);
 	params.center = bottom_center;
 	disk(&c->bottom, params);
 	c->print = &print_cylinder_capped;
 }
 
-void cylinder_mat_capped(t_cylinder_capped *c, t_init_params params)
+void	cylinder_mat_capped(t_cylinder_capped *c, t_init_params params)
 {
+	t_vec3		axis;
+	t_point3	top_center;
+	t_point3	bottom_center;
+
 	c->base.hit = hit_cylinder_capped;
 	c->base.pdf_value = obj_pdf_value;
 	c->base.random = obj_random;
 	cylinder_mat_uncapped(&c->cylinder_uncapped, params);
-	
-	t_vec3 axis = unit_vector(params.normal);
-    t_point3 top_center = vec3add(params.center, vec3multscalar(axis, params.height / 2));
-    t_point3 bottom_center = vec3add(params.center, vec3multscalar(axis, -(params.height / 2)));
-    params.center = top_center;
+	axis = unit_vector(params.normal);
+	top_center = vec3add(params.center, vec3multscalar(axis, params.height
+				/ 2));
+	bottom_center = vec3add(params.center, vec3multscalar(axis, -(params.height
+					/ 2)));
+	params.center = top_center;
 	disk_mat(&c->top, params);
 	params.center = bottom_center;
 	disk_mat(&c->bottom, params);
 	c->print = &print_cylinder_capped;
 }
-
-
