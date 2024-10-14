aea008116f5fcc8fd697b5c99f88cd147f4c60b7
Laurent Brusa <wrmultitudes@gmail.com>
2024-10-14 14:29:29 +0200

    MOVEDrt files around

--- bonus_scenes.sh
+++ bonus_scenes.sh
@@ -4,18 +4,21 @@ echo "Running multiple miniRT sessions in parallel..."
 echo "This is the mandatory part wuth ambbient light scenes."
 
 # Run each miniRT command in the background and store the PIDs
-./miniRT scenes/deep_space1.rt &
+
+./miniRT scenes/deep_space6.rt &
 pid1=$!
 
-./miniRT scenes/deep_space4.rt &
+./miniRT scenes/deep_space5.rt &
+pid2=$!
+
+./miniRT scenes/deep_space3.rt &
 pid3=$!
 
-
-
-
+./miniRT scenes/deep_space2.rt &
+pid4=$!
 
 # Wait for all background processes to finish
-wait $pid1 $pid2 $pid3 
+wait $pid1 $pid2 $pid3 $pid4
 
 # Optionally, terminate all processes (if needed)
 # kill $pid1 $pid2 $pid3 $pid4 $pid5 $pid6 $pid7
--- bonus_scenes2.sh
+++ bonus_scenes2.sh
@@ -4,18 +4,12 @@ echo "Running multiple miniRT sessions in parallel..."
 echo "This is the mandatory part wuth ambbient light scenes."
 
 # Run each miniRT command in the background and store the PIDs
-./miniRT scenes/deep_space1.rt &
+./miniRT scenes/deep_space_ambient0.rt &
 pid1=$!
-
-./miniRT scenes/deep_space4.rt &
-pid3=$!
-
-
-
 
 
 # Wait for all background processes to finish
-wait $pid1 $pid2 $pid3 
+wait $pid1 
 
 # Optionally, terminate all processes (if needed)
 # kill $pid1 $pid2 $pid3 $pid4 $pid5 $pid6 $pid7
